8b54a4012783915d423a59edf4b65dfe5912f2d8
Hillary <hillary07@gmial.com>
2016-03-25 10:59:07 -0700

    getting compiling errors. Updat App component as based on Keg Component

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,12 +2,27 @@ import { Component } from 'angular2/core';
 
 @Component({
   selector: 'my-app',
+  // directives: [FoodListComponent],
   template: `
     <div class="container">
-      <h1>Skeleton Angular2 App!</h1>
+      <h1>Meal Item Tracker App!</h1>
+      <food-list [foodList]="foods"
+      (onFoodSelect)="foodWasSelected($event)">
+      </food-list>
     <div>
   `
 })
-export class AppComponent {
 
+export class AppComponent {
+  public foods: Food[];
+  constructor() {
+    this.foods = [
+      new Food("Cobb Salad", "no egg", 800, 0),
+      new Food("Apple Pie", "A La Mode!", 900, 1),
+      new Food("Oatmeal", "1 cup, Made with brown sugar", 325, 2),
+      new Food("Pineapple", "1 cup", 130, 3)
+    ];
+  }
+  foodWasSelected(clickedFood: Food): void {
+  }
 }
